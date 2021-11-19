61931969c16d3e60825ff1ebf5ee3fb7215300d2
Mauricio Diaz <mrcdz@pm.me>
2021-11-18 21:05:37 -0700

    Add constructors to class Point

--- Point.cs
+++ Point.cs
@@ -6,6 +6,16 @@ namespace Activity5._3._1
     {
         public double x;
         public double y;
-        
+
+        public Point()
+        {
+
+        }
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
     }
 }
